82ee3973678563cc6858b155467476f8737af00a
Philip Nogueira <phlpnogueira@gmail.com>
2024-03-31 19:09:40 -0300

    tarefa módulo 9

---
+++ Tarefa_Wrapper/src/Tarefa/Tarefa.java
@@ -0,0 +1,17 @@
+package Tarefa;
+
+public class Tarefa {
+
+
+    public static void main(String arg[]){
+
+        long num1 = 123456789l;
+
+        System.out.println(num1);
+
+        Long meuInteger = num1;
+
+        System.out.println(meuInteger);
+    }
+
+}
